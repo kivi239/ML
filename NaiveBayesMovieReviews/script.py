a92beb1b54ab39f5800a5b4ef126cdd171d84427
kivi239 <kivi239@gmail.com>
2016-02-15 11:56:09 +0300

    Fix classifier, calculation of probability

--- NaiveBayesMovieReviews/script.py
+++ NaiveBayesMovieReviews/script.py
@@ -7,37 +7,44 @@ with open('good_texts.txt') as good_file:
 with open('neutral_texts.txt') as neutral_file:
     neutral = neutral_file.readlines()
 
+
+def count_words(class_data):
+    cnt = 0
+    for review in class_data:
+        for w in review.split(" "):
+           cnt += 1
+    return cnt
+
 all_texts = bad + good + neutral
 print("Please, enter your review\n")
 review = input()
 
 def prob_of_word(word, class_data):
-    count_all = 0
-    for review in all_texts:
-        for w in review.split(" "):
-            if (w == word):
-                count_all += 1
     count_class = 0
     for review in class_data:
         for w in review.split(" "):
             if (w == word):
                 count_class += 1
 
-    if (count_all == 0):
-        return 1
-
-    return count_class / count_all
-
-
+    count_all_words = count_words(class_data)
+    return (count_class + 1) / (count_all_words + 1)
 
 def calc_probability(class_data, review):
-    prob = 1 #len(class_data) / len(all_texts)
-    print(prob)
+    prob = len(class_data) / len(all_texts)
     for word in review.split(" "):
         prob *= prob_of_word(word, class_data)
     return prob
 
-print("Unnormalized probability that it is a good review:    %.3lf\n" % (calc_probability(good, review)))
-print("Unnormalized probability that it is a bad review:     %.3lf\n" % (calc_probability(bad, review)))
-print("Unnormalized probability that it is a neutral review: %.3lf\n" % (calc_probability(neutral, review)))
+prob_good = calc_probability(good, review)
+prob_bad = calc_probability(bad, review)
+prob_neutral = calc_probability(neutral, review)
+print("Unnormalized probability that it is a good review:    %.15lf\n" % (prob_good))
+print("Unnormalized probability that it is a bad review:     %.15lf\n" % (prob_bad))
+print("Unnormalized probability that it is a neutral review: %.15lf\n" % (prob_neutral))
 
+if (prob_good > prob_bad and prob_good > prob_neutral):
+    print("Good review")
+if (prob_bad > prob_good and prob_bad > prob_neutral):
+    print("Bad review")
+if (prob_neutral > prob_bad and prob_neutral > prob_good):
+    print("Neutral review")
