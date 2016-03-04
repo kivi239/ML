09dbc80e181289207de04af87a430c12fd9ef03a
kivi239 <kivi239@gmail.com>
2016-03-04 12:50:34 +0300

    Fix tabs

--- ClustSimple/clust.cpp
+++ ClustSimple/clust.cpp
@@ -14,73 +14,73 @@ const int Size = 2;
 
 // returns the id of new center according to the centers distribution
 int random_id(float *distribution, int n) {
-	float *prefix = new float[n + 1];
-	prefix[0] = 0;
-	for (int i = 1; i <= n; i++) 
-	 	prefix[i] = prefix[i - 1] + distribution[i - 1];
-	
-	float p = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/prefix[n]));
-	int l = 0, r = n;
-	int m;
-	while (l + 1 < r) {
-		m = (l + r) / 2;
-		if (prefix[m] > p)
-			r = m;
-		if (prefix[m + 1] <= p)
-			l = m;
-		if (prefix[m] <= p && p < prefix[m + 1])
-			break;
-	}
+  float *prefix = new float[n + 1];
+  prefix[0] = 0;
+  for (int i = 1; i <= n; i++) 
+    prefix[i] = prefix[i - 1] + distribution[i - 1];
+  
+  float p = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/prefix[n]));
+  int l = 0, r = n;
+  int m;
+  while (l + 1 < r) {
+    m = (l + r) / 2;
+    if (prefix[m] > p)
+      r = m;
+    if (prefix[m + 1] <= p)
+      l = m;
+    if (prefix[m] <= p && p < prefix[m + 1])
+      break;
+  }
 
-	return m;
+  return m;
 }
 
 // for centers initialization
 void K_meanspp(Vec *centers, int n) {
-	FILE *f = fopen("sample2d.in", "r");
-	int id_first = rand() % n;
-	int cur_id = 0;
-	Vec *vec = new Vec();
-	for (int i = 0; i <= id_first; i++) { 
-		delete vec;
-		vec = new Vec(Size, f);	
-	}
+  FILE *f = fopen("sample2d.in", "r");
+  int id_first = rand() % n;
+  int cur_id = 0;
+  Vec *vec = new Vec();
+  for (int i = 0; i <= id_first; i++) { 
+    delete vec;
+    vec = new Vec(Size, f); 
+  }
 
-	fclose(f);
-	centers[0] = *vec;
+  fclose(f);
+  centers[0] = *vec;
 
-	float *distribution = new float[n];
+  float *distribution = new float[n];
 
-	for (int step = 1; step < K; step++) {
-	  for (int i = 0; i < n; i++) {
-	  	vec = new Vec(Size, f);
-	  	distribution[i] = vec->mindist(centers, step);
-	    delete vec;
-	  }
+  for (int step = 1; step < K; step++) {
+    for (int i = 0; i < n; i++) {
+      vec = new Vec(Size, f);
+      distribution[i] = vec->mindist(centers, step);
+      delete vec;
+    }
 
-	  int center_id = random_id(distribution, n);
-	  f = fopen(file_name, "r");
-	  for (int i = 0; i <= center_id; i++)
-	  	vec = new Vec(Size, f);
-	  centers[step] = *vec;	
-	}
+    int center_id = random_id(distribution, n);
+    f = fopen(file_name, "r");
+    for (int i = 0; i <= center_id; i++)
+      vec = new Vec(Size, f);
+    centers[step] = *vec; 
+  }
 
-	delete[] distribution;
+  delete[] distribution;
 }
 
 
 int main() {
-	srand(time(NULL));
+  srand(time(NULL));
   FILE *f = fopen(file_name, "r");
 
   int n = 0;
   char *tmp;
   while (fscanf(f, "  %s  ", &tmp) != EOF) {
      n++;
-	}
-	fclose(f);
-	
-	n /= Size;
+  }
+  fclose(f);
+  
+  n /= Size;
     
   int *ids = new int[n];
   Vec *centers = new Vec[K];
@@ -90,7 +90,7 @@ int main() {
   freopen("centers.txt", "w", stdout);
   K_meanspp(centers, n);
   for (int i = 0; i < K; i++)
-  	centers[i].print();
+    centers[i].print();
 
 
   return 0;
--- ClustSimple/gen.cpp
+++ ClustSimple/gen.cpp
@@ -5,36 +5,36 @@
 const int N = 100;
 
 int main() {
-	freopen("sample2d.in", "w", stdout);
-	//srand(time(NULL))
+  freopen("sample2d.in", "w", stdout);
+  //srand(time(NULL))
 
-	for (int i = 0; i < N / 4; i++) {
-		double x = (rand() % 10000) * 1.0 / 100;
-		double y = (rand() % 10000) * 1.0 / 100;
+  for (int i = 0; i < N / 4; i++) {
+    double x = (rand() % 10000) * 1.0 / 100;
+    double y = (rand() % 10000) * 1.0 / 100;
 
-		printf("%.4lf %.4lf\n", x, y);
-	}
+    printf("%.4lf %.4lf\n", x, y);
+  }
 
-	for (int i = 0; i < N / 4; i++) {
-	  double x = (rand() % 3000) * 1.0 / 100;
-	  double y = (rand() % 3000) * 1.0 / 100;
-	   
-	  printf("%.4lf %.4lf\n", x, y);
-	}
+  for (int i = 0; i < N / 4; i++) {
+    double x = (rand() % 3000) * 1.0 / 100;
+    double y = (rand() % 3000) * 1.0 / 100;
+     
+    printf("%.4lf %.4lf\n", x, y);
+  }
 
-	for (int i = 0; i < N / 4; i++) {
-	  double x = (rand() % 3000) * 1.0 / 100 + 50;
-	  double y = (rand() % 3000) * 1.0 / 100 + 70;
-	   
-	  printf("%.4lf %.4lf\n", x, y);
-	}
+  for (int i = 0; i < N / 4; i++) {
+    double x = (rand() % 3000) * 1.0 / 100 + 50;
+    double y = (rand() % 3000) * 1.0 / 100 + 70;
+     
+    printf("%.4lf %.4lf\n", x, y);
+  }
 
-	for (int i = 0; i < N / 4; i++) {
-	  double x = (rand() % 3000) * 1.0 / 100;
-	  double y = (rand() % 1000) * 1.0 / 100 + 80;
-	   
-	  printf("%.4lf %.4lf\n", x, y);
-	}
+  for (int i = 0; i < N / 4; i++) {
+    double x = (rand() % 3000) * 1.0 / 100;
+    double y = (rand() % 1000) * 1.0 / 100 + 80;
+     
+    printf("%.4lf %.4lf\n", x, y);
+  }
 
-	return 0;
+  return 0;
 }
