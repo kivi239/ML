afecb9b8daf56d5a69e44caacd8bd6d17e941390
kivi239 <kivi239@gmail.com>
2016-04-01 19:38:11 +0300

    Normalize word-vectors! Move .txt data to other folder

--- Clust/script.py
+++ Clust/script.py
@@ -1,11 +1,37 @@
+# coding=utf-8
+import math
 import gensim
 import pymorphy2
+import time
 from sklearn.cluster import MiniBatchKMeans
+
+K = 5000
+D = 200
+
+
+def division(x, n):
+    for i in range(D):
+        x[i] /= n
+    return x
+
+
+def norm(x):
+    n = 0
+    for c in x:
+        n += c**2
+    return math.sqrt(n)
+
+
+t = time.time()
+
 print("Building model")
 
-model = gensim.models.Word2Vec.load_word2vec_format('../Word2Vec/all.s300.w11.n1.v30.cbow.bin', binary=True, unicode_errors='ignore')
+model = gensim.models.Word2Vec.load_word2vec_format('../Word2Vec/all.s200.w11.n1.v20.cbow.bin', binary=True, unicode_errors='ignore')
 
 print("Built model")
+
+print(time.time() - t)
+t = time.time()
 
 clust = MiniBatchKMeans(n_clusters=5000, init='k-means++', max_iter=60, batch_size=10000)
 
@@ -17,14 +43,18 @@ morph = pymorphy2.MorphAnalyzer()
 
 for word in model.vocab.keys():
     if word == morph.parse(word)[0].normal_form:
-        data.append(model[word])
+        x = model[word]
         words.append(word)
+        vec_norm = norm(x)
+        data.append(division(x, vec_norm))
+
 
 print("Start clustering, %d words" % len(words))
 
 res = clust.fit(data)
 
 print("finish clustering")
+print(time.time() - t)
 
 f = open('clusters_all_5000_2.txt', 'w')
 
@@ -35,12 +65,4 @@ for i in range(len(data)):
         f.write('\n')
     except UnicodeEncodeError:
         print("can't encode")
-
-
-
-
-
-
-
-
 
