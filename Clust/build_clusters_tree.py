3beef780df8a97aec4f5a0eb69e9841350e0abb7
kivi239 <kivi239@gmail.com>
2016-04-02 15:48:20 +0300

    FIX: update minimum distances after merging

--- Clust/build_clusters_tree.py
+++ Clust/build_clusters_tree.py
@@ -9,10 +9,12 @@ centers = [None] * (2 * K)
 for i in range(2 * K):
     centers[i] = [0.0] * D
 
-clusters_size = [0] * K
+clusters_size = [0] * (2 * K)
 
+# for every cluster save the nearest cluster and the distance between them
 min_dist = [(INF, -1)] * (2 * K)
 
+# for every node (cluster) save links to its 2 children
 tree = [None] * (2 * K)
 
 active_clusters = [True] * (2 * K)
@@ -71,6 +73,14 @@ def merge_clusters(j, k):
     active_clusters[j] = False
     active_clusters[k] = False
 
+
+# minimum for tuples
+def min(t1, t2):
+    if t1[0] < t2[0]:
+        return t1
+
+    return t2
+
 print("Building model")
 
 model = gensim.models.Word2Vec.load_word2vec_format('../Word2Vec/all.s200.w11.n1.v20.cbow.bin', binary=True, unicode_errors='ignore')
@@ -78,6 +88,7 @@ model = gensim.models.Word2Vec.load_word2vec_format('../Word2Vec/all.s200.w11.n1
 print("Built model")
 
 
+cnt = 0
 with open('clusters/clusters_all.txt') as f:
     for line in f:
         data = line.split(" ")
@@ -87,7 +98,10 @@ with open('clusters/clusters_all.txt') as f:
         vec_norm = norm(x)
         add(centers[cluster_id], division(x, vec_norm))
         clusters_size[cluster_id] += 1
+        cnt += 1
 
+        if cnt % 100000 == 0:
+            print("Load %d words\n" % cnt)
 
 print("Load clusters")
 
@@ -122,6 +136,21 @@ for it in range(K):
     print(min_tuple)
     merge_clusters(min_tuple[0], min_tuple[1])
     cnt_clusters += 1
+
+    for i in range(2 * K):
+        if not active_clusters[i]:
+            continue
+        if not active_clusters[min_dist[i][1]]:
+            min_dist[i] = (INF, -1)
+            for j in range(2 * K):
+                if not active_clusters[j]:
+                    continue
+                if i == j:
+                    continue
+
+                d = cosine_distance(centers[i], centers[j])
+                min_dist[i] = min(min_dist, (d, j))
+
 
 print("Built clusters tree")
 f = open("clusters/cluster_tree.txt", "w")
